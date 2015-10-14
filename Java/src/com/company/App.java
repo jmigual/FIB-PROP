a55eec1f0ffdafae4caaeefe030c51607d90a718
jmigual <j.marce.igual@gmail.com>
2015-10-14 17:57:30 +0200

    Changed java files to begin implementation and added more uml documentation

--- Java/src/com/company/App.java
+++
@@ -1,7 +0,0 @@
-package domini;
-
-/**
- * Created by Joan on 23/09/2015.
- */
-public class App {
-}
--- Java/src/com/company/KenKen.java
+++
@@ -1,9 +0,0 @@
-package domini;
-
-public class KenKen extends App {
-
-    public static void main(String[] args) {
-        // write your code here
-        System.out.print("Hello world");
-    }
-}
