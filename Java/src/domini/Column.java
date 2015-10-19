6d5400575bfb04e22200848a689afd06be341f3e
jmigual <j.marce.igual@gmail.com>
2015-10-19 22:22:23 +0200

    (IÑI) coses varies

--- Java/src/domini/Column.java
+++ Java/src/domini/Column.java
@@ -4,4 +4,11 @@ package domini;
  * Created by Joan on 15/10/2015.
  */
 public class Column {
+    private int size;
+    public Column (int size){
+        this.size=size;
+    }
+    public int size (){
+        return size;
+    }
 }
