9b7baeec1fbf16617988832751112c08547b1296
acanya <arnau_canya@hotmail.com>
2015-11-05 09:55:42 +0100

    small improvements

--- Java/src/domini/BoardCreator/CpuBoardCreator.java
+++ Java/src/domini/BoardCreator/CpuBoardCreator.java
@@ -1,15 +1,20 @@
 package domini.BoardCreator;
 
+import domini.Basic.Region;
 import sun.plugin2.gluegen.runtime.CPU;
+
+import java.util.ArrayList;
 
 /**
  * Created by arnau_000 on 05/11/2015.
  */
 public class CpuBoardCreator extends BoardCreator {
-
+    int mMaxRegionSize;
+    ArrayList<ArrayList<Region>> mRegionShapes;
 
     public CpuBoardCreator(int size){
         super(size);
     }
-    
+
+
 }
