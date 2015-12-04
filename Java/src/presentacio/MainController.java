9dd0bb4800d5605447d6771a5de8f7663bf9aa75
ImInSpace <ignigomoreno@hotmail.com>
2015-12-04 22:50:21 +0100

    Moved controller to mainController

---
+++ Java/src/presentacio/MainController.java
@@ -0,0 +1,42 @@
+package presentacio;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+/**
+ * Created by Inigo on 04/12/2015.
+ */
+public class MainController extends AnchorPane {
+
+    @FXML
+    private Pane leftArea;
+    private AnchorPane rootlayout;
+    public MainController(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            rootlayout = loader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    public Pane getLeftArea() {
+        return leftArea;
+    }
+
+    public AnchorPane getRootlayout() {
+        return rootlayout;
+    }
+
+    public void exit() {
+        Platform.exit();
+    }
+}
--- Java/src/presentacio/MainWindow.java
+++ Java/src/presentacio/MainWindow.java
@@ -8,15 +8,12 @@ import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
-import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.*;
@@ -26,16 +23,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainWindow extends Application implements Initializable{
+public class MainWindow extends Application {
 
     private Stage primaryStage;
     private AnchorPane rootLayout;
-    private Canvas canvas;
-    private GraphicsContext gc;
     private GridPane gridPane;
-    boolean initialized=false;
-    @FXML public Pane leftArea;
-    @FXML public MenuItem hola;
+    private Pane leftArea;
 
     public static void main(String[] args) {
         launch(args);
@@ -50,50 +43,44 @@ public class MainWindow extends Application implements Initializable{
     }
 
     private void initRootLayout() {
-        try {
-            // Load root layout from xml file
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainWindow.class.getResource("./MainWindow.fxml"));
-            rootLayout = loader.load();
+        // Load root layout from xml file
+        MainController mainController = new MainController();
+        leftArea = mainController.getLeftArea();
+        rootLayout = mainController.getRootlayout();
 
-            // Show the scene containing the root layout
-            Scene scene = new Scene(rootLayout);
-            addResizeListeners(scene);
-            canvas = new Canvas (scene.getWidth(),scene.getHeight());
-            gc = canvas.getGraphicsContext2D();
+        // Show the scene containing the root layout
+        Scene scene = new Scene(rootLayout);
+        addResizeListeners(scene);
 
-            primaryStage.setScene(scene);
-            primaryStage.show();
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
     }
-    private void addGrid(){
-        gridPane=new GridPane();
+
+    private void addGrid() {
+        gridPane = new GridPane();
         gridPane.setGridLinesVisible(true);
 
         rootLayout.getChildren().add(gridPane);
 
-        double size=9;
+        double size = 9;
 
         ColumnConstraints columnConstraints = new ColumnConstraints();
-        columnConstraints.setPercentWidth(100/size);
+        columnConstraints.setPercentWidth(100 / size);
         columnConstraints.setHalignment(HPos.CENTER);
 
         RowConstraints rowConstraints = new RowConstraints();
-        rowConstraints.setPercentHeight(100/size);
+        rowConstraints.setPercentHeight(100 / size);
         rowConstraints.setValignment(VPos.CENTER);
 
 
-        for (int i=0; i<size; i++){
+        for (int i = 0; i < size; i++) {
             gridPane.getColumnConstraints().add(columnConstraints);
             gridPane.getRowConstraints().add(rowConstraints);
         }
-        for (int i=0; i<size; i++){
-            for (int j=0; j<size; j++){
-                gridPane.add(new Label(Integer.toString(i)+Integer.toString(j)), i, j);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                gridPane.add(new Label(Integer.toString(i) + Integer.toString(j)), i, j);
             }
         }
 
@@ -116,13 +103,12 @@ public class MainWindow extends Application implements Initializable{
         });
     }
 
-    private void resized(){
-        if (initialized)System.out.println(leftArea.getHeight());
+    private void resized() {
+        System.out.println(leftArea.getHeight());
+
+        addGrid();
     }
 
-    public void exit() {
-        Platform.exit();
-    }
 
     /**
      * Called to initialize a controller after its root element has been
@@ -132,8 +118,4 @@ public class MainWindow extends Application implements Initializable{
      *                  <tt>null</tt> if the location is not known.
      * @param resources The resources used to localize the root object, or <tt>null</tt> if
      */
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        addGrid();
-    }
 }
